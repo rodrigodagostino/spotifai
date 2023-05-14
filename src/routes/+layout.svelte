4cc59fd2412405491563bf93442833182da7805c
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-05-14 19:16:45 -0300

    feat: Logout using new endpoint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
   import type { LayoutData } from './$types'
+  import ButtonLogout from '$components/ButtonLogout.svelte'
   import './styles.css'
 
   export let data: LayoutData
+
+  $: user = data.user
 </script>
 
-{data.user?.display_name}
+{#if user}
+  <p>Hello, {data.user?.display_name}</p>
+  <ButtonLogout />
+{/if}
 
 <slot />
---
+++ src/routes/api/auth/logout/+server.ts
@@ -0,0 +1,13 @@
+import { json, redirect } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+
+export const POST: RequestHandler = ({ cookies, request }) => {
+  cookies.delete('access_token', { path: '/' })
+  cookies.delete('refresh_token', { path: '/' })
+
+  if (request.headers.get('accept') === 'application/json') {
+    return json({ success: true })
+  }
+
+  throw redirect(303, '/login')
+}
