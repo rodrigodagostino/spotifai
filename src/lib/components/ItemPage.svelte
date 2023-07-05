a75149d304fba520a30a9a1a3be048074e451f20
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-07-05 00:18:55 -0300

    feat: Create ItemPage component

---
+++ src/lib/components/ItemPage.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { IconMusic } from '@tabler/icons-svelte'
+
+  export let color: string | null
+  export let image: string | undefined
+  export let title: string
+  export let type: string | undefined
+</script>
+
+<div class="item-page">
+  <header class="item-page__header">
+    {#if image}
+      <img class="item-page__image" src={image} alt={title} loading="lazy" />
+    {:else}
+      <div class="item-page__image-placeholder"><IconMusic size={80} /></div>
+    {/if}
+    <div class="item-page__info">
+      {#if type}<p class="item-page__type">{type}</p>{/if}
+      <h1 class="item-page__title">{title}</h1>
+      <slot name="meta" />
+    </div>
+  </header>
+  <div class="item-page__content"><slot /></div>
+</div>
--- src/routes/album/[id]/+page.svelte
+++ src/routes/album/[id]/+page.svelte
@@ -1,25 +1,39 @@
 <script lang="ts">
   import type { PageData } from './$types'
   import getCopyrightSymbol from '$components/helpers/copyright-symbol'
+  import ItemPage from '$components/ItemPage.svelte'
 
   export let data: PageData
 
   $: album = data.album
 </script>
 
-<h1>{album.name}</h1>
-
-<ul class="album__tracks">
-  {#each album.tracks.items as track}
-    <li>{track.name}</li>
-  {/each}
-</ul>
-
-<div class="album__credits">
-  <p class="album__release-date">
-    {new Date(album.release_date).toLocaleDateString('en', { dateStyle: 'medium' })}
+<ItemPage
+  title={album.name}
+  type={album.album_type}
+  color={null}
+  image={album.images.length ? album.images[0].url : undefined}
+>
+  <p class="album__meta" slot="meta">
+    <span class="album__artists">{album.artists.map((artist) => artist.name).join(', ')}</span>
+    <span class="album__date">{new Date(album.release_date).getFullYear()}</span>
+    <span class="album__total-tracks">
+      {album.total_tracks}
+      {album.total_tracks === 1 ? 'song' : 'songs'}
+    </span>
   </p>
-  {#each album.copyrights as copyright}
-    <p class="album__copyright">{getCopyrightSymbol(copyright.type)}{copyright.text}</p>
-  {/each}
-</div>
+  <ul class="album__tracks">
+    {#each album.tracks.items as track}
+      <li>{track.name}</li>
+    {/each}
+  </ul>
+
+  <div class="album__credits">
+    <p class="album__release-date">
+      {new Date(album.release_date).toLocaleDateString('en', { dateStyle: 'medium' })}
+    </p>
+    {#each album.copyrights as copyright}
+      <p class="album__copyright">{getCopyrightSymbol(copyright.type)}{copyright.text}</p>
+    {/each}
+  </div>
+</ItemPage>
