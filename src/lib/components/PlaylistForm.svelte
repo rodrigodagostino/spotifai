febe88751a1738d8212bbd6bbcbc03575cd5ba60
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-07-25 00:33:34 -0300

    refactor: Move Add Playlist form to separate component

---
+++ src/lib/components/PlaylistForm.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import type { ActionData as AddActionData } from '../../routes/playlists/add/$types';
+  import Button from './Button.svelte';
+
+  export let form: AddActionData;
+  export let userId: string | undefined = undefined;
+  export let action: string | undefined = undefined;
+</script>
+
+<form class="playlist-form" method="POST" {action}>
+  {#if userId}<input hidden name="userId" value={userId} />{/if}
+  <div class="form-field" class:form-field--has-error={form?.nameError}>
+    <label for="playlist-name">Name</label>
+    <input type="text" id="playlist-name" name="name" value={form?.name || ''} />
+    {#if form?.nameError}
+      <p class="form-field__error">{form?.nameError}</p>
+    {/if}
+  </div>
+  <div class="form-field">
+    <label for="playlist-description">Description</label>
+    <input
+      type="text"
+      id="playlist-description"
+      name="description"
+      value={form?.description || ''}
+    />
+  </div>
+  {#if form?.apiError}
+    <p class="form__error">{form?.apiError}</p>
+  {/if}
+  <Button element="button" type="submit">Create playlist</Button>
+</form>
+
+<style lang="scss">
+  .playlist-form {
+    display: flex;
+    flex-direction: column;
+    gap: 1.25rem;
+    width: 100%;
+    max-width: 22.5rem;
+
+    :global(.button) {
+      align-self: center;
+      margin-top: 1rem;
+    }
+  }
+
+  .form-field {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    input {
+      padding: 0.5rem;
+      border: none;
+      border-radius: 0.25rem;
+      background-color: var(--gray-750);
+    }
+
+    &__error {
+      font-size: 0.875rem;
+      color: var(--red-500);
+    }
+
+    &--has-error {
+      input {
+        box-shadow: inset 0 0 0 0.125rem var(--red-500);
+      }
+    }
+  }
+</style>
--- src/routes/playlists/add/+page.svelte
+++ src/routes/playlists/add/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Button from '$components/Button.svelte';
+  import PlaylistForm from '$components/PlaylistForm.svelte';
 
   export let data;
   export let form;
@@ -10,61 +10,17 @@
     <h1>Add a new playlist</h1>
   </header>
   <div class="add-playlist-page__content">
-    <form class="form" method="POST">
-      <input hidden name="userId" value={data.user?.id} />
-      <div class="form-field" class:form-field--has-error={form?.nameError}>
-        <label for="playlist-name">Name</label>
-        <input type="text" id="playlist-name" name="name" value={form?.name || ''} />
-        {#if form?.nameError}
-          <p class="form-field__error">{form?.nameError}</p>
-        {/if}
-      </div>
-      <div class="form-field">
-        <label for="playlist-description">Description</label>
-        <input
-          type="text"
-          id="playlist-description"
-          name="description"
-          value={form?.description || ''}
-        />
-      </div>
-      {#if form?.apiError}
-        <p class="form__error">{form?.apiError}</p>
-      {/if}
-      <Button element="button" type="submit">Create playlist</Button>
-    </form>
+    <PlaylistForm userId={data.user?.id} {form} />
   </div>
 </div>
 
 <style lang="scss">
   .add-playlist-page {
     &__content {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
       margin-top: 2rem;
-    }
-  }
-
-  .form {
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    gap: 1.25rem;
-  }
-
-  .form-field {
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-
-    input {
-      padding: 0.5rem;
-      border: none;
-      border-radius: 0.25rem;
-      background-color: var(--gray-750);
-    }
-
-    &__error {
-      font-size: 0.875rem;
-      color: var(--red-500);
     }
   }
 </style>
