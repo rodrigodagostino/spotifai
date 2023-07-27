90934b5e48aa53c2e11116df95ecec038218e6b5
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-07-27 13:45:36 -0300

    chore: Update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,13 @@ module.exports = {
     es2017: true,
     node: true,
   },
+  /**
+   * In order to display Prettier errors through ESLint, 'plugin:prettier/recommended' is added in the property below.
+   * Switch it for 'prettier' and remove 'eslint-plugin-prettier' from 'plugins' if you do not wish this behavior
+   * (the 'eslint-plugin-prettier' dependency can be removed too).
+   */
   extends: ['plugin:svelte/prettier', 'plugin:prettier/recommended'],
+  plugins: ['eslint-plugin-prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
