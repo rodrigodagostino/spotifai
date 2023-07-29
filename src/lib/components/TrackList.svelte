700f2c985133a646e59ed7830a3d935513f65f9c
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-07-29 03:31:36 -0300

    feat: Extract TrackListItem to separate component and add context menu

--- src/lib/components/TrackList.svelte
+++ src/lib/components/TrackList.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-  import { IconClock, IconPlayerPauseFilled, IconPlayerPlayFilled } from '@tabler/icons-svelte';
+  import { IconClock } from '@tabler/icons-svelte';
 
-  import Button from '$components/Button.svelte';
-  import { setActiveTrack, setPaused, navigation } from '$stores/navigation';
-  import msToTime from '$helpers/ms-to-time';
-  import playingAnimation from '$assets/images/playing-animation.gif';
+  import TrackListItem from './TrackListItem.svelte';
 
   export let tracks: SpotifyApi.TrackObjectFull[] | SpotifyApi.TrackObjectSimplified[];
   export let type:
     | SpotifyApi.AlbumObjectSimplified['album_type']
     | SpotifyApi.PlaylistBaseObject['type'];
+
+  let currentIndex: number;
 </script>
 
 <div class="track-list__labels">
@@ -23,61 +22,9 @@
     <span class="track-list__labels__duration-label"><IconClock size={18} /></span>
   </div>
 </div>
-<ul class="track-list__tracks">
-  {#each tracks as track, i}
-    <li
-      class="track-list__track"
-      class:is-active={track.id === $navigation.activeTrack?.id}
-      class:is-paused={track.id === $navigation.activeTrack?.id && $navigation.paused}
-    >
-      <div class="track-list__track__column">
-        <span class="track-list__track__number">
-          {type === 'album' && track.track_number ? track.track_number : i + 1}
-        </span>
-        {#if track.id === $navigation.activeTrack?.id && !$navigation.paused}
-          <Button
-            element="button"
-            variant="icon-ghost"
-            aria-label="Pause {track.name}"
-            on:click={() => setPaused(true)}
-          >
-            <IconPlayerPauseFilled size={16} />
-          </Button>
-        {:else}
-          <Button
-            element="button"
-            variant="icon-ghost"
-            aria-label="Play {track.name}"
-            on:click={() => setActiveTrack(track)}
-          >
-            <IconPlayerPlayFilled size={16} />
-          </Button>
-        {/if}
-        <img class="track-list__track__animation" src={playingAnimation} alt="" />
-      </div>
-      <div class="track-list__track__column">
-        <audio class="track-list__track__player" src={track.preview_url} controls />
-        <div class="track-list__track__info">
-          <span class="track-list__track__name">
-            {track.name}
-            {#if track.explicit}
-              <span class="track-list__track__explicit">Explicit</span>
-            {/if}
-          </span>
-          <div class="track-list__track__artists">
-            {#each track.artists as artist, i}
-              <a href="/artist/{artist.id}" class="track-list__track__artist">{artist.name}</a>
-              {#if i < track.artists.length - 1}<span>,&nbsp;</span>{/if}
-            {/each}
-          </div>
-        </div>
-      </div>
-      <div class="track-list__track__column">
-        <span class="track-list__track__duration">
-          {msToTime(track.duration_ms)}
-        </span>
-      </div>
-    </li>
+<ul class="track-list__items">
+  {#each tracks as track, index}
+    <TrackListItem {type} {track} {index} bind:currentIndex />
   {/each}
 </ul>
 
@@ -85,10 +32,10 @@
   .track-list {
     &__labels {
       display: grid;
-      grid-template-columns: 1rem 4fr auto;
+      grid-template-columns: 2rem 4fr 2.125rem;
       column-gap: 1.25rem;
       align-items: center;
-      padding: 0.5rem 1rem;
+      padding: 0.25rem 1rem 0.25rem 0.75rem;
       margin-bottom: 1rem;
       border-bottom: 1px solid hsla(0, 0%, 100%, 0.2);
 
@@ -99,7 +46,7 @@
 
         &:first-child,
         &:last-child {
-          align-items: end;
+          align-items: center;
         }
       }
 
@@ -111,167 +58,8 @@
       }
     }
 
-    &__tracks {
+    &__items {
       list-style: none;
-    }
-
-    &__track {
-      display: grid;
-      grid-template-columns: 1rem 4fr 2.125rem;
-      column-gap: 1.25rem;
-      align-items: center;
-      padding: 0.25rem 1rem;
-      border-radius: 0.25rem;
-
-      &:focus-within,
-      &:hover,
-      &.is-active {
-        background-color: hsla(0, 0%, 100%, 0.1);
-
-        .track-list__track__number {
-          opacity: 0;
-
-          & + :global(.button) {
-            opacity: 1;
-          }
-        }
-      }
-
-      &.is-active {
-        &:not(:focus-within):not(:hover) {
-          .track-list__track {
-            &__number + :global(.button) {
-              display: none;
-            }
-          }
-
-          &:not(.is-paused) {
-            .track-list__track {
-              &__animation {
-                display: inline-block;
-              }
-            }
-          }
-        }
-
-        .track-list__track {
-          &__number {
-            color: var(--spotify-text);
-          }
-
-          &__name {
-            color: var(--spotify-text);
-          }
-        }
-      }
-
-      &.is-paused {
-        &:not(:focus-within):not(:hover) {
-          .track-list__track {
-            &__number {
-              opacity: 1;
-            }
-          }
-        }
-      }
-
-      &__column {
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        position: relative;
-
-        &:first-child,
-        &:last-child {
-          align-items: end;
-        }
-      }
-
-      &__number {
-        color: var(--gray-300);
-
-        & + :global(.button) {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-40%, -50%);
-          opacity: 0;
-        }
-      }
-
-      &__animation {
-        display: none;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-      }
-
-      &__explicit {
-        padding: 0 0.25rem;
-        margin-left: 0.25rem;
-        border: 1px solid var(--gray-300);
-        border-radius: 0.25rem;
-        vertical-align: middle;
-        font-size: 0.625rem;
-        color: var(--gray-300);
-        text-transform: uppercase;
-      }
-
-      &__artists {
-        display: flex;
-        flex-wrap: wrap;
-        font-size: 0.8125rem;
-        color: var(--gray-300);
-      }
-
-      &__artist {
-        color: var(--gray-300);
-        text-decoration: none;
-
-        &:focus,
-        &:hover {
-          color: var(--white);
-          text-decoration: underline;
-        }
-      }
-
-      &__player {
-        display: none;
-      }
-
-      &__duration {
-        color: var(--gray-300);
-      }
-    }
-  }
-
-  :global(.no-js) {
-    .track-list {
-      &__track {
-        &:focus-within,
-        &:hover,
-        &.is-active {
-          .track-list__track__number {
-            opacity: 1;
-
-            & + :global(.button) {
-              opacity: 0;
-            }
-          }
-        }
-
-        &__column:nth-child(2) {
-          flex-direction: row;
-          align-items: center;
-          gap: 1rem;
-        }
-
-        &__player {
-          display: initial;
-          max-width: 32%;
-        }
-      }
     }
   }
 </style>
---
+++ src/lib/components/TrackListItem.svelte
@@ -0,0 +1,335 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import {
+    IconCaretRightFilled,
+    IconPlayerPauseFilled,
+    IconPlayerPlayFilled,
+  } from '@tabler/icons-svelte';
+
+  import { navigation, setActiveTrack, setPaused } from '$stores/navigation';
+  import msToTime from '$helpers/ms-to-time';
+  import Button from './Button.svelte';
+  import playingAnimation from '$assets/images/playing-animation.gif';
+
+  export let type:
+    | SpotifyApi.AlbumObjectSimplified['album_type']
+    | SpotifyApi.PlaylistBaseObject['type'];
+  export let track: SpotifyApi.TrackObjectFull | SpotifyApi.TrackObjectSimplified;
+  export let index: number;
+  export let currentIndex: number;
+
+  let trackRef: HTMLLIElement;
+  let menuX = 0;
+  let menuY = 0;
+  let isHidden = true;
+
+  $: if (currentIndex !== index) isHidden = true;
+
+  const handleContextMenu = (event: MouseEvent) => {
+    event.preventDefault();
+    currentIndex = index;
+
+    const menu = trackRef?.querySelector<HTMLUListElement>('.track-list-item__menu');
+    const layerX = event.layerX;
+    const layerY = event.layerY;
+
+    if (!!track && !!menu) {
+      menuX =
+        layerX + menu.offsetWidth * 2 <= trackRef.offsetWidth ? layerX : layerX - menu.offsetWidth;
+      menuY = layerY;
+      isHidden = false;
+    }
+  };
+
+  const handleClick = (event: MouseEvent) => {
+    if (!trackRef || !trackRef.contains(event?.target as Node)) isHidden = true;
+  };
+
+  onMount(() => {
+    document.addEventListener('click', handleClick);
+    document.addEventListener('contextmenu', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+      document.removeEventListener('contextmenu', handleClick);
+    };
+  });
+</script>
+
+<li
+  bind:this={trackRef}
+  class="track-list-item"
+  class:is-active={track.id === $navigation.activeTrack?.id}
+  class:is-paused={track.id === $navigation.activeTrack?.id && $navigation.paused}
+  on:contextmenu={handleContextMenu}
+>
+  <div class="track-list-item__column">
+    <span class="track-list-item__number">
+      {type === 'album' && track.track_number ? track.track_number : index + 1}
+    </span>
+    {#if track.id === $navigation.activeTrack?.id && !$navigation.paused}
+      <Button
+        element="button"
+        variant="icon-ghost"
+        aria-label="Pause {track.name}"
+        on:click={() => setPaused(true)}
+      >
+        <IconPlayerPauseFilled size={16} />
+      </Button>
+    {:else}
+      <Button
+        element="button"
+        variant="icon-ghost"
+        aria-label="Play {track.name}"
+        on:click={() => setActiveTrack(track)}
+      >
+        <IconPlayerPlayFilled size={16} />
+      </Button>
+    {/if}
+    <img class="track-list-item__animation" src={playingAnimation} alt="" />
+  </div>
+  <div class="track-list-item__column">
+    <audio class="track-list-item__player" src={track.preview_url} controls />
+    <div class="track-list-item__info">
+      <span class="track-list-item__name">
+        {track.name}
+        {#if track.explicit}
+          <span class="track-list-item__explicit">Explicit</span>
+        {/if}
+      </span>
+      <div class="track-list-item__artists">
+        {#each track.artists as artist, i}
+          <a href="/artist/{artist.id}" class="track-list-item__artist">{artist.name}</a>
+          {#if i < track.artists.length - 1}<span>,&nbsp;</span>{/if}
+        {/each}
+      </div>
+    </div>
+  </div>
+  <div class="track-list-item__column">
+    <span class="track-list-item__duration">
+      {msToTime(track.duration_ms)}
+    </span>
+  </div>
+  <ul class="track-list-item__menu" style="top: {menuY}px; left: {menuX}px" aria-hidden={isHidden}>
+    <li>
+      Add to playlist
+      <IconCaretRightFilled size={16} />
+      <ul class="track-list-item__submenu">
+        <li>Playlist 1</li>
+        <li>Playlist 2</li>
+        <li>Playlist 3</li>
+        <li>Playlist 4</li>
+      </ul>
+    </li>
+  </ul>
+</li>
+
+<style lang="scss">
+  .track-list-item {
+    display: grid;
+    grid-template-columns: 2rem 4fr 2.125rem;
+    column-gap: 1.25rem;
+    align-items: center;
+    padding: 0.25rem 1rem 0.25rem 0.75rem;
+    border-radius: 0.25rem;
+    position: relative;
+
+    &:focus-within,
+    &:hover,
+    &.is-active {
+      background-color: hsla(0, 0%, 100%, 0.1);
+
+      .track-list-item__number {
+        display: none;
+
+        & + :global(.button) {
+          display: inline-flex;
+        }
+      }
+    }
+
+    &.is-active {
+      &:not(:focus-within):not(:hover) {
+        .track-list-item {
+          &__number + :global(.button) {
+            display: none;
+          }
+        }
+
+        &:not(.is-paused) {
+          .track-list-item {
+            &__animation {
+              display: inline-block;
+            }
+          }
+        }
+      }
+
+      .track-list-item {
+        &__number {
+          color: var(--spotify-text);
+        }
+
+        &__name {
+          color: var(--spotify-text);
+        }
+      }
+    }
+
+    &.is-paused {
+      &:not(:focus-within):not(:hover) {
+        .track-list-item {
+          &__number {
+            display: inline-block;
+          }
+        }
+      }
+    }
+
+    &__column {
+      display: grid;
+      width: 100%;
+
+      &:first-child,
+      &:nth-child(3) {
+        justify-items: center;
+        align-items: center;
+      }
+    }
+
+    &__number {
+      grid-column: 1/1;
+      grid-row: 1;
+      color: var(--gray-300);
+
+      & + :global(.button) {
+        display: none;
+        grid-column: 1/1;
+        grid-row: 1;
+      }
+    }
+
+    &__animation {
+      display: none;
+      grid-column: 1/1;
+      grid-row: 1;
+      width: 1.25rem;
+    }
+
+    &__menu,
+    &__submenu {
+      min-width: 12rem;
+      padding: 0.25rem;
+      border-radius: 0.25rem;
+      background-color: var(--gray-850);
+      box-shadow:
+        0 0 1px rgba(0, 0, 0, 0.16),
+        0 0.25rem 0.75rem rgba(0, 0, 0, 0.2);
+    }
+
+    &__menu {
+      position: absolute;
+      list-style: none;
+      z-index: 1;
+
+      &[aria-hidden='true'] {
+        display: none;
+      }
+
+      &[aria-hidden='false'] {
+        display: block;
+      }
+
+      li {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0.5rem 0.5rem 0.5rem 0.75rem;
+        border-radius: 0.125rem;
+        white-space: nowrap;
+        position: relative;
+
+        &:focus-within,
+        &:hover {
+          background-color: var(--gray-750);
+
+          .track-list-item__submenu {
+            display: block;
+          }
+        }
+      }
+    }
+
+    &__submenu {
+      display: none;
+      position: absolute;
+      top: 50%;
+      left: 100%;
+      transform: translateY(-50%);
+      list-style: none;
+    }
+
+    &__explicit {
+      padding: 0 0.25rem;
+      margin-left: 0.25rem;
+      border: 1px solid var(--gray-300);
+      border-radius: 0.25rem;
+      vertical-align: middle;
+      font-size: 0.625rem;
+      color: var(--gray-300);
+      text-transform: uppercase;
+    }
+
+    &__artists {
+      display: flex;
+      flex-wrap: wrap;
+      font-size: 0.8125rem;
+      color: var(--gray-300);
+    }
+
+    &__artist {
+      color: var(--gray-300);
+      text-decoration: none;
+
+      &:focus,
+      &:hover {
+        color: var(--white);
+        text-decoration: underline;
+      }
+    }
+
+    &__player {
+      display: none;
+    }
+
+    &__duration {
+      color: var(--gray-300);
+    }
+  }
+
+  :global(.no-js) {
+    .track-list-item {
+      &:focus-within,
+      &:hover,
+      &.is-active {
+        .track-list-item__number {
+          opacity: 1;
+
+          & + :global(.button) {
+            opacity: 0;
+          }
+        }
+      }
+
+      &__column:nth-child(2) {
+        flex-direction: row;
+        align-items: center;
+        gap: 1rem;
+      }
+
+      &__player {
+        display: initial;
+        max-width: 32%;
+      }
+    }
+  }
+</style>
