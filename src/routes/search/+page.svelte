5d9508f76752f059c138369ffa88d7c81704f1ca
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2023-08-01 19:00:17 -0300

    feat: Create Search page

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,1 +1,70 @@
-<h1>Search</h1>
+<script lang="ts">
+  import type { PageData } from './$types';
+  import Grid from '$components/Grid.svelte';
+
+  export let data: PageData;
+
+  $: categories = data.categories?.categories.items;
+</script>
+
+<div class="playlists-page">
+  <header class="search-page__header">
+    <h1 class="search-page__title sr-only">Search</h1>
+  </header>
+  {#if categories}
+    <section class="search-page__section">
+      <header class="search-page__section__header">
+        <h2 class="search-page__section__title">Browse all</h2>
+      </header>
+      <Grid>
+        {#each categories as category}
+          <a class="search-page__category" href="/category/{category.id}">
+            {#if category.icons.length > 0}
+              <img src={category.icons[0].url} alt={category.name} />
+            {/if}
+            <h3 class="search-page__category__name">{category.name}</h3>
+          </a>
+        {/each}
+      </Grid>
+    </section>
+  {/if}
+</div>
+
+<style lang="scss">
+  .search-page {
+    &__section {
+      display: flex;
+      flex-direction: column;
+      gap: 1.25rem;
+    }
+
+    &__section__header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    &__category {
+      border-radius: 0.5rem;
+      text-decoration: none;
+      overflow: hidden;
+      position: relative;
+    }
+
+    &__category__name {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 88%;
+      height: 3rem;
+      position: absolute;
+      left: 50%;
+      bottom: 8%;
+      transform: translateX(-50%);
+      font-size: 1.25rem;
+      font-weight: 600;
+      line-height: 1.2;
+      text-align: center;
+    }
+  }
+</style>
--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -1,7 +1,12 @@
+import fetchRefresh from '$helpers/fetch-refresh';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = () => {
+export const load: PageLoad = async ({ fetch }) => {
+  const categoriesResponse = await fetchRefresh(fetch, '/api/spotify/browse/categories?limit=50');
+  const categoriesResponseJSON: SpotifyApi.MultipleCategoriesResponse | undefined =
+    categoriesResponse.ok ? await categoriesResponse.json() : undefined;
+
   return {
-    title: 'Search',
+    categories: categoriesResponseJSON,
   };
 };
